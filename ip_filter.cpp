6059aca666f047be5cf440340fcdcbc4d559c8aa
albkharisov1 <albkharisov@gmail.com>
2018-10-23 00:07:24 +0300

    DONE

--- ip_filter.cpp
+++ ip_filter.cpp
@@ -7,6 +7,8 @@
 
 using ip_pool_t = std::vector<std::vector<std::string>>;
 
+#define IPV4_PARTS      4
+
 template <typename... Args>
 auto filter(const ip_pool_t &ip_pool, Args... args)
 {
@@ -15,30 +17,44 @@ auto filter(const ip_pool_t &ip_pool, Args... args)
     ip_pool_t ipf;  // filtered
     for (auto ip : ip_pool)
     {
-        std::cout << "ip:" << ip[0] << "." << ip[1] << "." << ip[2] << "." << ip[3] << std::endl;
+//        std::cout << "ip:" << ip[0] << "." << ip[1] << "." << ip[2] << "." << ip[3] << std::endl;
         bool rc = true;
         int b = 0;
         for (auto j : a)
         {
-            std::cout << "    ip[j]:" << ip[b] << " | " << std::to_string(j) << std::endl;
-            if (ip[b] != std::to_string(j))
+//            std::cout << "    ip[j]:" << ip[b] << " | " << std::to_string(j) << std::endl;
+//            if (ip[b] != std::to_string(j))
+            if (std::stoi(ip[b]) != j)
             {
                 rc = false;
                 break;
             }
-            if (++b > 4)
+            if (++b > IPV4_PARTS)
                 break;
         }
         if (rc)
-        {
-            std::cout << "success" << std::endl;
             ipf.push_back(ip);
-        }
     }
 
     return ipf;
 }
 
+auto filter_any(const ip_pool_t &ip_pool, unsigned char num)
+{
+    ip_pool_t ipf;  // filtered
+    for (auto ip : ip_pool)
+    {
+        for (int i = 0 ; i < IPV4_PARTS ; ++i)
+        {
+            if (ip[i] == std::to_string(num))
+            {
+                ipf.push_back(ip);
+                break;
+            }
+        }
+    }
+    return ipf;
+}
 
 // ("",  '.') -> [""]
 // ("11", '.') -> ["11"]
@@ -91,11 +107,9 @@ int main(int argc, char const *argv[])
     (void) argc;    // suppress warnings
     (void) argv;    // suppress warnings
 
-    ip_pool_t ip_pool;
-
     try
     {
-
+        ip_pool_t ip_pool;
         for(std::string line; std::getline(std::cin, line);)
         {
             auto v = split(line, '\t');
@@ -108,76 +122,16 @@ int main(int argc, char const *argv[])
         printIpPool(ip_pool);
         std::cout << "sort" << std::endl;
 
+        std::cout << "    filter:1:" << std::endl;
         auto ipfiltered = filter(ip_pool, 1);
         printIpPool(ipfiltered);
-        ipfiltered = filter(ip_pool, 46, 70);
+        std::cout << "    filter:46.70:" << std::endl;
+        ipfiltered = filter(ip_pool, 46, 70);       // std::move implicitly, do we need to overload?
         printIpPool(ipfiltered);
-//        ipfiltered = filter(ip_pool, 46, 70);        // std::move implicitly
-//        auto ip4670 = filter_any(ip_pool, 46);
-//        printIpPool(ip_pool);               // std::move implicitly
+        std::cout << "    filter_any:46:" << std::endl;
+        ipfiltered = filter_any(ip_pool, 46);       // std::move implicitly
+        printIpPool(ipfiltered);
 
-        // 222.173.235.246
-        // 222.130.177.64
-        // 222.82.198.61
-        // ...
-        // 1.70.44.170
-        // 1.29.168.152
-        // 1.1.234.8
-
-        // TODO filter by first byte and output
-        // ip = filter(1)
-
-        // 1.231.69.33
-        // 1.87.203.225
-        // 1.70.44.170
-        // 1.29.168.152
-        // 1.1.234.8
-
-        // TODO filter by first and second bytes and output
-        // ip = filter(46, 70)
-
-        // 46.70.225.39
-        // 46.70.147.26
-        // 46.70.113.73
-        // 46.70.29.76
-
-        // TODO filter by any byte and output
-        // ip = filter_any(46)
-
-        // 186.204.34.46
-        // 186.46.222.194
-        // 185.46.87.231
-        // 185.46.86.132
-        // 185.46.86.131
-        // 185.46.86.131
-        // 185.46.86.22
-        // 185.46.85.204
-        // 185.46.85.78
-        // 68.46.218.208
-        // 46.251.197.23
-        // 46.223.254.56
-        // 46.223.254.56
-        // 46.182.19.219
-        // 46.161.63.66
-        // 46.161.61.51
-        // 46.161.60.92
-        // 46.161.60.35
-        // 46.161.58.202
-        // 46.161.56.241
-        // 46.161.56.203
-        // 46.161.56.174
-        // 46.161.56.106
-        // 46.161.56.106
-        // 46.101.163.119
-        // 46.101.127.145
-        // 46.70.225.39
-        // 46.70.147.26
-        // 46.70.113.73
-        // 46.70.29.76
-        // 46.55.46.98
-        // 46.49.43.85
-        // 39.46.86.85
-        // 5.189.203.46
     }
     catch(const std::exception &e)
     {
