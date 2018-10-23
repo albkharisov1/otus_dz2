2412f755820148db4b5cc59f80a00112dcc9e825
albkharisov1 <albkharisov@gmail.com>
2018-10-24 00:37:20 +0300

    another step to victory

--- ip_filter.h
+++ ip_filter.h
@@ -1,8 +1,8 @@
 #pragma once
 
-//#include <cassert>
-//#include <cstdlib>
-//#include <iostream>
+#include <cassert>
+#include <cstdlib>
+#include <iostream>
 #include <string>
 #include <vector>
 #include <algorithm>
@@ -12,8 +12,51 @@ using ip_pool_t = std::vector<ip_t>;
 
 #define IPV4_PARTS      4
 
-ip_pool_t filter_any(const ip_pool_t &ip_pool, unsigned char num);
+ip_pool_t filter_any(const ip_pool_t &ip_pool, unsigned char num)
+{
+    ip_pool_t ipf;  // filtered
+    for (auto ip : ip_pool)
+    {
+        for (int i = 0 ; i < IPV4_PARTS ; ++i)
+        {
+            if (ip[i] == std::to_string(num))
+            {
+                ipf.push_back(ip);
+                break;
+            }
+        }
+    }
+    return ipf;
+}
 
 template <typename... Args>
-ip_pool_t filter(const ip_pool_t &ip_pool, Args... args);
+ip_pool_t filter(const ip_pool_t &ip_pool, Args... args)
+{
+    const int a[] = {args...};
+    static_assert(sizeof...(Args) <= 4);
+
+    ip_pool_t ipf;  // filtered
+    for (auto ip : ip_pool)
+    {
+//        std::cout << "ip:" << ip[0] << "." << ip[1] << "." << ip[2] << "." << ip[3] << std::endl;
+        bool rc = true;
+        int b = 0;
+        for (auto j : a)
+        {
+//            std::cout << "    ip[j]:" << ip[b] << " | " << std::to_string(j) << std::endl;
+//            if (ip[b] != std::to_string(j))
+            if (std::stoi(ip[b]) != j)
+            {
+                rc = false;
+                break;
+            }
+            if (++b > IPV4_PARTS)
+                break;
+        }
+        if (rc)
+            ipf.push_back(ip);
+    }
+
+    return ipf;
+}
 
